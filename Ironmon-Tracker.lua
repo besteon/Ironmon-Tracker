6da1d9e8f32bc4617c06a34d6bbccd2b72718903
OnlySpaghettiCode <brian.neveu2@gmail.com>
2022-07-17 21:26:38 -0400

    remove call to util method since unneeded now

--- Ironmon-Tracker.lua
+++ Ironmon-Tracker.lua
@@ -76,7 +76,6 @@ function Main.Run()
 	else
 		Tracker.loadData()
 		Buttons.initializeBadgeButtons()
-		Utils.figureOutPokemonStadiumExtension()
 		client.SetGameExtraPadding(0, GraphicConstants.UP_GAP, GraphicConstants.RIGHT_GAP, GraphicConstants.DOWN_GAP)
 		gui.defaultTextBackground(0)
 
