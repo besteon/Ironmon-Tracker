53f94690f7ef04ce4d1bf7269e9cfea3c67874a8
thisisatest1900 <thisisatest1990@protonmail.com>
2022-08-02 17:30:19 +0300

    spain clean up and fixed few wrong addresses

--- ironmon_tracker/GameSettings.lua
+++ ironmon_tracker/GameSettings.lua
@@ -618,25 +618,25 @@ end
 function GameSettings.setGameAsFireRedSpanish(gameversion)
 	if gameversion == 0x005A0000 then
 		print("ROM Detected: Pokemon Rojo Fuego")
-		GameSettings.gBaseStats = 0x082547f4
+		GameSettings.gBaseStats = 0x0824ff4c
 		GameSettings.sMonSummaryScreen = 0x0203b140
-		GameSettings.sSpecialFlags = 0x020370e0 -- not sure if its the real value used for.its used for rse only anyway so not that important
-		GameSettings.sBattlerAbilities = 0x02039a30 --not used in tracker so no idea
+		GameSettings.sSpecialFlags = 0x020370e0
+		GameSettings.sBattlerAbilities = 0x02039a30
 		GameSettings.gBattlerAttacker = 0x02023d6b
 		GameSettings.gBattlerPartyIndexesSelfSlotOne = 0x02023bce
 		GameSettings.gBattlerPartyIndexesEnemySlotOne = GameSettings.gBattlerPartyIndexesSelfSlotOne + 0x2
 		GameSettings.gBattlerPartyIndexesSelfSlotTwo = GameSettings.gBattlerPartyIndexesSelfSlotOne + 0x4 --not tested
 		GameSettings.gBattlerPartyIndexesEnemySlotTwo = GameSettings.gBattlerPartyIndexesSelfSlotOne + 0x6 -- not tested
 		GameSettings.gBattleMons = 0x02023be4
-		GameSettings.gBattlescriptCurrInstr = 0x02023d74 --seems like they are same as fr but not sure
-		--this section is not tested but everything was the same so lets hope 
-		GameSettings.BattleScript_FocusPunchSetUp = 0x081d9085 + 0x10 -- TODO: offset for this game is untested
-		GameSettings.BattleScript_LearnMoveLoop = 0x081d8a81
-		GameSettings.BattleScript_LearnMoveReturn = 0x081d8ad3
+		GameSettings.gBattlescriptCurrInstr = 0x02023d74
+		
+		GameSettings.BattleScript_FocusPunchSetUp = 0x081d8b47 + 0x10 -- TODO: offset for this game is untested
+		GameSettings.BattleScript_LearnMoveLoop = 0x081D8543
+		GameSettings.BattleScript_LearnMoveReturn = 0x081D8595
 		GameSettings.gMoveToLearn = 0x02024022
 		GameSettings.gBattleOutcome = 0x02023e8a
 
-		GameSettings.FriendshipRequiredToEvo = 0x08042ED8 + 0x13E -- GetEvolutionTargetSpecies (untested)
+		GameSettings.FriendshipRequiredToEvo = 0x08042db0 + 0x13E -- GetEvolutionTargetSpecies (untested)
 
 		--the only diffrance looks like in here gSaveBlock1ptr and gSaveBlock2ptr
 		GameSettings.gSaveBlock1ptr = 0x03004F58
