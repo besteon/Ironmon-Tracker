7902e29477efd94f18794a0fbb479d223bd4995f
Fellshadow <fellshadow97@gmail.com>
2022-08-04 10:51:14 +0100

    Update LeafGreen v1.1 ability scripts

--- ironmon_tracker/GameSettings.lua
+++ ironmon_tracker/GameSettings.lua
@@ -351,6 +351,7 @@ function GameSettings.setGameAsSapphire(gameversion)
 end
 
 function GameSettings.setGameAsEmerald()
+	-- https://raw.githubusercontent.com/pret/pokeemerald/symbols/pokeemerald.sym
 	print("ROM Detected: Pokemon Emerald")
 
 	GameSettings.gBaseStats = 0x083203cc
@@ -384,7 +385,6 @@ function GameSettings.setGameAsEmerald()
 	GameSettings.bagPocket_Items_Size = 30
 	GameSettings.bagPocket_Berries_Size = 46
 
-	-- https://raw.githubusercontent.com/pret/pokeemerald/symbols/pokeemerald.sym
 	GameSettings.ABILITIES = {
 		[0x082db430] = 2, -- BattleScript_DrizzleActivates + 0x0 Drizzle
 		[0x082db44b] = 3, -- BattleScript_SpeedBoostActivates + 0x7 Speed Boost
@@ -416,6 +416,7 @@ end
 
 function GameSettings.setGameAsFireRed(gameversion)
 	if gameversion == 0x01670000 then
+		-- https://raw.githubusercontent.com/pret/pokefirered/symbols/pokefirered_rev1.sym
 		print("ROM Detected: Pokemon Fire Red v1.1")
 
 		GameSettings.gBaseStats = 0x082547f4
@@ -453,7 +454,6 @@ function GameSettings.setGameAsFireRed(gameversion)
 		GameSettings.bagPocket_Items_Size = 42
 		GameSettings.bagPocket_Berries_Size = 43
 
-		-- https://raw.githubusercontent.com/pret/pokefirered/symbols/pokefirered_rev1.sym
 		GameSettings.ABILITIES = {
 			BATTLER = { -- Abilities where we can use gBattleScripting.battler to determine enemy/player
 				[0x081d92ef] = {[2]  = true}, -- BattleScript_DrizzleActivates + 0x0 Drizzle
@@ -562,6 +562,7 @@ function GameSettings.setGameAsFireRed(gameversion)
 			},
 		}
 	elseif gameversion == 0x00680000 then
+		-- https://raw.githubusercontent.com/pret/pokefirered/symbols/pokefirered.sym
 		print("ROM Detected: Pokemon Fire Red v1.0")
 
 		GameSettings.gBaseStats = 0x08254784
@@ -599,7 +600,6 @@ function GameSettings.setGameAsFireRed(gameversion)
 		GameSettings.bagPocket_Items_Size = 42
 		GameSettings.bagPocket_Berries_Size = 43
 
-		-- https://raw.githubusercontent.com/pret/pokefirered/symbols/pokefirered.sym
 		GameSettings.ABILITIES = {
 			BATTLER = { -- Abiliities where we can use gBattleScripting.battler to determine enemy/player
 				[0x081d927f] = {[2]  = true}, -- BattleScript_DrizzleActivates + 0x0 Drizzle
@@ -713,6 +713,7 @@ end
 function GameSettings.setGameAsFireRedItaly(gameversion)
 	if gameversion == 0x00640000 then
 		print("ROM Detected: Pokemon - Rosso Fuoco")
+
 		GameSettings.gBaseStats = 0x0824d864
 		GameSettings.sMonSummaryScreen = 0x0203b140
 		GameSettings.sSpecialFlags = 0x020370e0
@@ -731,9 +732,9 @@ function GameSettings.setGameAsFireRedItaly(gameversion)
 		GameSettings.BattleScript_LearnMoveReturn = 0x081D5ECD -- expect them to not always be right
 		GameSettings.gMoveToLearn = 0x02024022
 		GameSettings.gBattleOutcome = 0x02023e8a
-
+		
 		GameSettings.FriendshipRequiredToEvo = 0x08042db0 + 0x13E -- GetEvolutionTargetSpecies (untested)
-
+		
 		--the only diffrance looks like in here gSaveBlock1ptr and gSaveBlock2ptr
 		GameSettings.gSaveBlock1ptr = 0x03004F58
 		GameSettings.gSaveBlock2ptr = 0x03004F5C
@@ -744,7 +745,7 @@ function GameSettings.setGameAsFireRedItaly(gameversion)
 		GameSettings.bagPocket_Berries_offset = 0x54c
 		GameSettings.bagPocket_Items_Size = 42
 		GameSettings.bagPocket_Berries_Size = 43
-
+		
 		-- https://raw.githubusercontent.com/pret/pokefirered/symbols/pokefirered_rev1.sym
 		--base for abilitys is fire red v1.1 looks like diff between abilitys is same
 		--this was not stright forward to get so expect to find errors here 
@@ -783,6 +784,7 @@ end
 function GameSettings.setGameAsFireRedSpanish(gameversion)
 	if gameversion == 0x005A0000 then
 		print("ROM Detected: Pokemon Rojo Fuego")
+
 		GameSettings.gBaseStats = 0x0824ff4c
 		GameSettings.sMonSummaryScreen = 0x0203b140
 		GameSettings.sSpecialFlags = 0x020370e0
@@ -801,9 +803,9 @@ function GameSettings.setGameAsFireRedSpanish(gameversion)
 		GameSettings.BattleScript_LearnMoveReturn = 0x081D8595
 		GameSettings.gMoveToLearn = 0x02024022
 		GameSettings.gBattleOutcome = 0x02023e8a
-
+		
 		GameSettings.FriendshipRequiredToEvo = 0x08042db0 + 0x13E -- GetEvolutionTargetSpecies (untested)
-
+		
 		--the only diffrance looks like in here gSaveBlock1ptr and gSaveBlock2ptr
 		GameSettings.gSaveBlock1ptr = 0x03004F58
 		GameSettings.gSaveBlock2ptr = 0x03004F5C
@@ -814,7 +816,7 @@ function GameSettings.setGameAsFireRedSpanish(gameversion)
 		GameSettings.bagPocket_Berries_offset = 0x54c
 		GameSettings.bagPocket_Items_Size = 42
 		GameSettings.bagPocket_Berries_Size = 43
-
+		
 		-- https://raw.githubusercontent.com/pret/pokefirered/symbols/pokefirered_rev1.sym
 		--base for abilitys is fire red v1.1 looks like diff between abilitys is same
 		--so take what you get from firered v1.v and find where drizzle is
@@ -853,6 +855,7 @@ end
 function GameSettings.setGameAsFireRedFrench(gameversion)
 	if gameversion == 0x00670000 then
 		print("ROM Detected: Pokemon Rouge Feu")
+
 		GameSettings.gBaseStats = 0x0824ebd4
 		GameSettings.sMonSummaryScreen = 0x0203b140
 		GameSettings.sSpecialFlags = 0x020370e0
@@ -922,6 +925,7 @@ end
 
 function GameSettings.setGameAsLeafGreen(gameversion)
 	if gameversion == 0x01800000 then
+		-- https://raw.githubusercontent.com/pret/pokefirered/symbols/pokeleafgreen_rev1.sym
 		print("ROM Detected: Pokemon Leaf Green v1.1")
 
 		GameSettings.gBaseStats = 0x082547d0
@@ -955,35 +959,115 @@ function GameSettings.setGameAsLeafGreen(gameversion)
 		GameSettings.bagPocket_Items_Size = 42
 		GameSettings.bagPocket_Berries_Size = 43
 
-		-- https://raw.githubusercontent.com/pret/pokefirered/symbols/pokeleafgreen_rev1.sym
 		GameSettings.ABILITIES = {
-			[0x081d92cb] = 2, -- BattleScript_DrizzleActivates + 0x0 Drizzle
-			[0x081d92e6] = 3, -- BattleScript_SpeedBoostActivates + 0x7 Speed Boost
-			[0x081d93ed] = 5, -- BattleScript_SturdyPreventsOHKO + 0x0 Sturdy
-			[0x081d93fb] = 6, -- BattleScript_DampStopsExplosion + 0x0 Damp
-			[0x081d7291] = 7, -- BattleScript_LimberProtected + 0x0 Limber (untested)
-			[0x081d9490] = 12, -- BattleScript_ObliviousPreventsAttraction + 0x0 Oblivious (untested)
-			[0x081d94eb] = 16, -- BattleScript_ColorChangeActivates + 0x3 Color Change
-			[0x081d6e9b] = 17, -- BattleScript_ImmunityProtected + 0x0 Immunity (untested)
-			[0x081d9446] = 18, -- BattleScript_FlashFireBoost + 0x3 Flash Fire
-			[0x081d94ac] = 20, -- BattleScript_OwnTempoPrevents + 0x0 Own Tempo
-			[0x081d9359] = 22, -- BattleScript_DoIntimidateActivationAnim + 0x0 Intimidate
-			[0x081d94ff] = 24, -- BattleScript_RoughSkinActivates + 0x10 Rough Skin
-			[0x081d951a] = 28, -- BattleScript_SynchronizeActivates + 0x0 Synchronize (untested)
-			[0x081d9462] = 29, -- BattleScript_AbilityNoStatLoss + 0x0 Clear Body & White Smoke
-			[0x081d92ed] = 36, -- BattleScript_TraceActivates + 0x0 Trace
-			[0x081d94c2] = 43, -- BattleScript_SoundproofProtected + 0x8 Soundproof
-			[0x081d92fa] = 44, -- BattleScript_RainDishActivates + 0x3 Rain Dish
-			[0x081d930b] = 45, -- BattleScript_SandstreamActivates + 0x0 Sand Stream
-			[0x081d94d0] = 52, -- BattleScript_AbilityNoSpecificStatLoss + 0x6 Hyper Cutter
-			[0x081d9543] = 54, -- BattleScript_MoveUsedLoafingAround + 0x5 Truant
-			[0x081d9513] = 56, -- BattleScript_CuteCharmActivates + 0x9 Cute Charm
-			[0x081d94da] = 60, -- BattleScript_StickyHoldActivates + 0x0 Sticky Hold
-			[0x081d9322] = 61, -- BattleScript_ShedSkinActivates + 0x3 Shed Skin
-			[0x081d93c5] = 70, -- BattleScript_DroughtActivates + 0x0 Drought
-			[0x081d6a20] = 72, -- BattleScript_CantMakeAsleep + 0x8 Vital Spirit
+			BATTLER = { -- Abiliities where we can use gBattleScripting.battler to determine enemy/player
+				[0x081d92cb] = {[2]  = true}, -- BattleScript_DrizzleActivates + 0x0 Drizzle
+				[0x081d92e6] = {[3]  = true}, -- BattleScript_SpeedBoostActivates + 0x7 Speed Boost
+				[0x081d93bd] = {[22] = true}, -- BattleScript_IntimidateAbilityFail + 0x6 Intimidate Fail
+				[0x081d93a5] = {[22] = true}, -- BattleScript_IntimidateActivationAnimLoop + 0x3d Intimidate Succeed
+				[0x081d92f3] = {[36] = true}, -- BattleScript_TraceActivates + 0x6 Trace
+				[0x081d930b] = {[45] = true}, -- BattleScript_SandstreamActivates + 0x0 Sand Stream
+				[0x081d9322] = {[61] = true}, -- BattleScript_ShedSkinActivates + 0x3 Shed Skin
+				[0x081d93c5] = {[70] = true}, -- BattleScript_DroughtActivates + 0x0 Drought
+				[0x081d9468] = { -- BattleScript_AbilityNoStatLoss + 0x6
+					[29] = true, -- Clear Body
+					[73] = true, -- White Smoke
+				},
+				[0x081d94d0] = { -- BattleScript_AbilityNoSpecificStatLoss + 0x6
+					[51] = true, -- Keen Eye 
+					[52] = true, -- Hyper Cutter
+				},
+			},
+			REVERSE_BATTLER = { -- Abilities like BATTLER, but with logic reversed
+				[0x081d93bd] = { -- BattleScript_IntimidateAbilityFail + 0x6
+					[29] = true, -- Clear Body
+					[52] = true, -- Hyper Cutter
+					[73] = true, -- White Smoke
+				},
+			},
+			ATTACKER = { -- Abilities where we can use gBattlerAttacker to determine enemy/player
+				[0x081d93f3] = {[5]  = true}, -- BattleScript_SturdyPreventsOHKO + 0x6 Sturdy
+				[0x081d9490] = {[12] = true}, -- BattleScript_ObliviousPreventsAttraction + 0x0 Oblivious
+				[0x081d94eb] = {[16] = true}, -- BattleScript_ColorChangeActivates + 0x3 Color Change
+				[0x081d944c] = {[18] = true}, -- BattleScript_FlashFireBoost + 0x9 Flash Fire
+				[0x081d94ac] = {[20] = true}, -- BattleScript_OwnTempoPrevents + 0x0 Own Tempo
+				[0x081d945a] = {[21] = true}, -- BattleScript_AbilityPreventsPhasingOut + 0x6 Suction Cups
+				[0x081d94ff] = {[24] = true}, -- BattleScript_RoughSkinActivates + 0x10 Rough Skin
+				[0x081d698f] = {[26] = true}, -- BattleScript_HitFromAtkAnimation + 0xF Levitate ; Actually checking gMoveResultFlags during this message
+				[0x081d9513] = {[56] = true}, -- BattleScript_CuteCharmActivates + 0x9 Cute Charm
+				[0x081d94da] = {[60] = true}, -- BattleScript_StickyHoldActivates + 0x0 Sticky Hold
+				[0x081d6a8b] = {[64] = true}, -- BattleScript_AbsorbUpdateHp + 0x14 Liquid Ooze (Drain Moves)
+				[0x081d941e] = { -- BattleScript_MoveHPDrain + 0x14 --> Ability heals HP
+					[10] = true, -- Volt Absorb
+					[11] = true, -- Water Absorb
+				},
+				[0x081d9434] = { -- BattleScript_MonMadeMoveUseless + 0x7 --> Ability nullifies move
+					[10] = true, -- Volt Absorb
+					[11] = true, -- Water Absorb
+				},
+				[0x081d9480] = { -- BattleScript_PRLZPrevention + 0x8 
+					[7]  = true, -- Limber
+					[28] = true, -- Synchronize (is unable to inflict paralysis on other mon)
+				},
+				[0x081d948c] = { -- BattleScript_PSNPrevention + 0x8
+					[17] = true, -- Immunity
+					[28] = true, -- Synchronize (is unable to inflict poison on other mon)
+				},
+				[0x081d9474] = { -- BattleScript_BRNPrevention + 0x8
+					[28] = true, -- Synchronize (is unable to inflict burn on other mon)
+					[41] = true, -- Water Veil
+				},
+				[0x081d6a20] = { -- BattleScript_CantMakeAsleep + 0x8 --> Ability blocks attacker from inflicting sleep
+					[15] = true, -- Insomnia
+					[72] = true, -- Vital Spirit
+				},
+			},
+			REVERSE_ATTACKER = { -- Abilities like the above ATTACKER checks, but logic is reversed
+				[0x081d92fa] = {[44] = true}, -- BattleScript_RainDishActivates + 0x3 Rain Dish
+				[0x081d9543] = {[54] = true}, -- BattleScript_MoveUsedLoafingAround + 0x5 Truant
+				[0x081d6f06] = { -- BattleScript_RestCantSleep + 0x8 --> Ability blocks mon's own rest attempt
+					[15] = true, -- Insomnia
+					[72] = true, -- Vital Spirit
+				},
+			},
+			STATUS_INFLICT = { -- Abilities which apply a status effect on the opposing mon
+				[0x081d920c] = {[27] = true}, -- BattleScript_MoveEffectSleep + 0x7 Effect Spore (Sleep)
+				[0x081d9255] = { -- BattleScript_MoveEffectParalysis + 0x7
+					[9]  = true, -- Static
+					[27] = true, -- Effect Spore
+					[28] = true, -- Synchronize
+				},
+				[0x081d9228] = { -- BattleScript_MoveEffectPoison + 0x7
+					[27] = true, -- Effect Spore
+					[28] = true, -- Synchronize
+					[38] = true, -- Poison Point
+				},
+				[0x081d9237] = { -- BattleScript_MoveEffectBurn + 0x7
+					[28] = true, -- Synchronize
+					[49] = true, -- Flame Body
+				},
+			},
+			BATTLE_TARGET = { -- Abilities where we can use gBattlerTarget to determine enemy/player
+				[0x081d9401] = { -- BattleScript_DampStopsExplosion + 0x6 Damp
+					[6] = true,
+					scope = "both",
+				},
+				[0x081d94c2] = { -- BattleScript_SoundproofProtected + 0x8 Soundproof 1
+					[43] = true,
+					scope = "self",
+				}, 
+				[0x081d7616] = { -- BattleScript_EffectHealBell + 0x29 Soundproof 2 (Enemy uses Heal Bell)
+					[43] = true,
+					scope = "self",
+				},
+				[0x081d8be3] = { -- BattleScript_LeechSeedTurnPrintAndUpdateHp + 0x12 Liquid Ooze (Leech Seed)
+					[64] = true,
+					scope = "other",
+				}, 
+			},
 		}
 	elseif gameversion == 0x00810000 then
+		-- https://raw.githubusercontent.com/pret/pokefirered/symbols/pokeleafgreen.sym
 		print("ROM Detected: Pokemon Leaf Green v1.0")
 
 		GameSettings.gBaseStats = 0x08254760
@@ -1017,7 +1101,6 @@ function GameSettings.setGameAsLeafGreen(gameversion)
 		GameSettings.bagPocket_Items_Size = 42
 		GameSettings.bagPocket_Berries_Size = 43
 
-		-- https://raw.githubusercontent.com/pret/pokefirered/symbols/pokeleafgreen.sym
 		GameSettings.ABILITIES = {
 			[0x081d925b] = 2, -- BattleScript_DrizzleActivates + 0x0 Drizzle
 			[0x081d9276] = 3, -- BattleScript_SpeedBoostActivates + 0x7 Speed Boost
