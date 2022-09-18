6c254c455790f03307315d86740d75345c95ce4c
Zac Elsik <shadowgripper@gmail.com>
2022-09-18 13:21:09 -0700

    Updating version number to 6.3.1
    Not a required hotfix to fix something broken, but mostly doing this to try out creating a release that hopefully doesn't break the new update feature.

--- Ironmon-Tracker.lua
+++ Ironmon-Tracker.lua
@@ -1,7 +1,7 @@
 Main = {}
 
 -- The latest version of the tracker. Should be updated with each PR.
-Main.Version = { major = "6", minor = "3", patch = "0" }
+Main.Version = { major = "6", minor = "3", patch = "1" }
 
 Main.CreditsList = { -- based on the PokemonBizhawkLua project by MKDasher
 	CreatedBy = "Besteon",
