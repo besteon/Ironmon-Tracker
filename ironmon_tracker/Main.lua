e815771a3557965d36798cbbc07363d7c74a3259
Zac Elsik <shadowgripper@gmail.com>
2022-11-09 14:27:40 -0800

    Removing a TODO comment that is now complete

--- ironmon_tracker/Main.lua
+++ ironmon_tracker/Main.lua
@@ -2,7 +2,6 @@ Main = {}
 
 -- The latest version of the tracker. Should be updated with each PR.
 Main.Version = { major = "1", minor = "0", patch = "0" }
--- TODO: Purposefully downgrading Tracker version to test auto-update code, the real version should get updated to 7.0.0 before official release
 
 Main.CreditsList = { -- based on the PokemonBizhawkLua project by MKDasher
 	CreatedBy = "Besteon",
