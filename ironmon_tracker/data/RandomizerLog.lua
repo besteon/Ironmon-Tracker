ae1399a5211a514a02529fd1c8a1d79927964850
Zac Elsik <shadowgripper@gmail.com>
2022-12-30 04:42:41 -0800

    Log Parser: Fixing compatibility with 4.4.0 logs
    Also significantly speeding up how fast it parses the logs: table.remove() was wildly inefficient

--- ironmon_tracker/data/RandomizerLog.lua
+++ ironmon_tracker/data/RandomizerLog.lua
@@ -2,7 +2,7 @@
 RandomizerLog = {}
 
 RandomizerLog.Patterns = {
-	RandomizerVersion = "Randomizer Version:%s*([%d%.]+)%s*$", -- Note, log file line 1 does NOT start with this
+	RandomizerVersion = "Randomizer Version:%s*([%d%.]+)%s*$", -- Note: log file line 1 does NOT start with "Rando..."
 	RandomizerSeed = "^Random Seed:%s*(%d+)%s*$",
 	RandomizerSettings = "^Settings String:%s*(.+)%s*$",
 	RandomizerGame = "^Randomization of%s*(.+)%s+completed",
@@ -14,40 +14,42 @@ RandomizerLog.Patterns = {
 	end,
 }
 
+-- Each Sector has Pattern(s) to match on, and the Sector Start Header LineNumber
 RandomizerLog.Sectors = {
 	-- First three lines: Randomizer Version, Random Seed, Settings String
+
 	Evolutions = {
-		Header = "Randomized Evolutions",
+		HeaderPattern = RandomizerLog.Patterns.getSectorHeaderPattern("Randomized Evolutions"),
 		-- Matches: pokemon, evos
 		PokemonEvosPattern = "^" .. RandomizerLog.Patterns.PokemonName .. "%s*%->%s*(.*)",
 	},
-	BaseStatsItems = { -- Currently unused, Get Base Stats from Movesets instead
-		Header = "Pokemon Base Stats & Types",
+	BaseStatsItems = {
+		HeaderPattern = RandomizerLog.Patterns.getSectorHeaderPattern("Pokemon Base Stats & Types"),
 		-- Matches: pokemon, hp, atk, def, spatk, spdef, spd, helditems
 		PokemonBSTPattern = "^.*|" .. RandomizerLog.Patterns.PokemonName .. "%s*|.*|%s*(%d*)|%s*(%d*)|%s*(%d*)|%s*(%d*)|%s*(%d*)|%s*(%d*)|.*|.*|(.*)",
 	},
 	MoveSets = {
-		Header = "Pokemon Movesets",
+		HeaderPattern = RandomizerLog.Patterns.getSectorHeaderPattern("Pokemon Movesets"),
 		-- Matches: pokemon
 		NextMonPattern = "^%d+%s" .. RandomizerLog.Patterns.PokemonName .. "%s*%->",
 		-- Matches: level, movename
 		MovePattern = "^Level%s(%d+)%s?%s?:%s(.*)",
 	},
 	TMMoves = {
-		Header = "TM Moves",
+		HeaderPattern = RandomizerLog.Patterns.getSectorHeaderPattern("TM Moves"),
 		-- Matches: tmNumber, movename
 		TMPattern = "^TM(%d+)%s(.*)",
 	},
+	-- TODO: If TMs aren't randomized, then we don't store the original TM data, we probably need to do that
 	TMCompatibility = {
-		-- TODO: If TMs aren't randomized, then we don't store the original TM data, we probably need to do that
-		Header = "TM Compatibility",
+		HeaderPattern = RandomizerLog.Patterns.getSectorHeaderPattern("TM Compatibility"),
 		-- Matches: pokemon
 		NextMonPattern = "^[%s%d]+%s" .. RandomizerLog.Patterns.PokemonName .. "%s*|(.*)",
 		-- Matches: tmNumber
 		TMPattern = "TM(%d+)",
 	},
 	Trainers = {
-		Header = "Trainers Pokemon",
+		HeaderPattern = RandomizerLog.Patterns.getSectorHeaderPattern("Trainers Pokemon"),
 		-- Matches: trainer_num, trainername, party
 		NextTrainerPattern = "^#(%d+)%s%((.+)%s*=>.*%s%-%s(.*)",
 		-- Matches: partypokemon (pokemon name with held item and level info)
@@ -57,13 +59,14 @@ RandomizerLog.Sectors = {
 	},
 	-- Currently unused
 	PickupItems = {
-		Header = "Pickup Items",
+		HeaderPattern = RandomizerLog.Patterns.getSectorHeaderPattern("Pickup Items"),
 		LevelPattern = "",
 		PercentPattern = "",
 		ItemsPattern = "",
 	},
 	GameInfo = {
-		Header = "%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-", -- :(
+		-- :(
+		HeaderPattern = RandomizerLog.Patterns.getSectorHeaderPattern("%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-"),
 	},
 }
 
@@ -111,8 +114,8 @@ function RandomizerLog.parseLog(filepath)
 	end
 
 	RandomizerLog.resetData()
+	RandomizerLog.locateSectorLineStarts(logLines)
 
-	-- Trying with an inefficient approach first, parsing the lines in order
 	RandomizerLog.setupMappings()
 	RandomizerLog.parseRandomizerSettings(logLines)
 	RandomizerLog.parseEvolutions(logLines)
@@ -157,27 +160,28 @@ function RandomizerLog.resetData()
 	end
 end
 
--- Locates the start of the sector, returning the index
-function RandomizerLog.seekSectorStart(logLines, sectorHead)
+-- Locates the starting line number of each sector
+function RandomizerLog.locateSectorLineStarts(logLines)
 	if logLines == nil or #logLines == 0 then
-		return false
+		return
 	end
 
-	local line = table.remove(logLines, 1)
-	while line ~= nil do
-		if string.find(line, sectorHead) ~= nil then
-			return true
+	for i, line in ipairs(logLines) do
+		-- If the sector hasn't been found yet, and its this line
+		for _, sector in pairs(RandomizerLog.Sectors) do
+			if sector.LineNumber == nil and string.find(line, sector.HeaderPattern) ~= nil then
+				sector.LineNumber = i + 1 -- +1 to skip the header itself
+				break -- and continue looking for other sectors
+			end
 		end
-		line = table.remove(logLines, 1)
 	end
-	return false
 end
 
 function RandomizerLog.parseRandomizerSettings(logLines)
 	if #logLines < 3 then return end
-	local version = string.match(table.remove(logLines, 1), RandomizerLog.Patterns.RandomizerVersion)
-	local randomSeed = string.match(table.remove(logLines, 1), RandomizerLog.Patterns.RandomizerSeed)
-	local settingsString = string.match(table.remove(logLines, 1), RandomizerLog.Patterns.RandomizerSettings)
+	local version = string.match(logLines[1], RandomizerLog.Patterns.RandomizerVersion)
+	local randomSeed = string.match(logLines[2], RandomizerLog.Patterns.RandomizerSeed)
+	local settingsString = string.match(logLines[3], RandomizerLog.Patterns.RandomizerSettings)
 	RandomizerLog.Data.Settings.Version = version
 	RandomizerLog.Data.Settings.RandomSeed = randomSeed
 	RandomizerLog.Data.Settings.SettingsString = settingsString
@@ -185,30 +189,27 @@ end
 
 -- This sector is near the end of the file
 function RandomizerLog.parseRandomizerGame(logLines)
-	local sectorHeader = RandomizerLog.Patterns.getSectorHeaderPattern(RandomizerLog.Sectors.GameInfo.Header)
-	if not RandomizerLog.seekSectorStart(logLines, sectorHeader) then
+	if RandomizerLog.Sectors.GameInfo.LineNumber == nil then
 		return
 	end
 
-	local game = string.match(table.remove(logLines, 1), RandomizerLog.Patterns.RandomizerGame)
+	local game = string.match(logLines[RandomizerLog.Sectors.GameInfo.LineNumber] or "", RandomizerLog.Patterns.RandomizerGame)
 	RandomizerLog.Data.Settings.Game = game
 end
 
 function RandomizerLog.parseEvolutions(logLines)
-	local sectorHeader = RandomizerLog.Patterns.getSectorHeaderPattern(RandomizerLog.Sectors.Evolutions.Header)
-	if not RandomizerLog.seekSectorStart(logLines, sectorHeader) then
+	if RandomizerLog.Sectors.Evolutions.LineNumber == nil then
 		return
 	end
 
 	-- Parse the sector
-	local line = table.remove(logLines, 1)
-	while line ~= nil do
-		local pokemon, evos = string.match(line, RandomizerLog.Sectors.Evolutions.PokemonEvosPattern)
+	local index = RandomizerLog.Sectors.Evolutions.LineNumber
+	while index <= #logLines do
+		local pokemon, evos = string.match(logLines[index] or "", RandomizerLog.Sectors.Evolutions.PokemonEvosPattern)
 		pokemon = RandomizerLog.formatInput(pokemon)
 
 		-- If nothing matches, end of sector
 		if pokemon == nil or evos == nil or RandomizerLog.PokemonNameToIdMap[pokemon] == nil then
-			table.insert(logLines, 1, line)
 			return
 		end
 
@@ -221,71 +222,65 @@ function RandomizerLog.parseEvolutions(logLines)
 				table.insert(RandomizerLog.Data.Pokemon[pokemonId].Evolutions, RandomizerLog.PokemonNameToIdMap[evo])
 			end
 		end
-		line = table.remove(logLines, 1)
+
+		index = index + 1
 	end
 end
 
 function RandomizerLog.parseBaseStatsItems(logLines)
-	local sectorHeader = RandomizerLog.Patterns.getSectorHeaderPattern(RandomizerLog.Sectors.BaseStatsItems.Header)
-	if not RandomizerLog.seekSectorStart(logLines, sectorHeader) then
+	if RandomizerLog.Sectors.BaseStatsItems.LineNumber == nil then
 		return
 	end
 
-	table.remove(logLines, 1) -- remove the first line to skip the table header
-
 	-- Parse the sector
-	local line = table.remove(logLines, 1)
-	while line ~= nil do
-		local pokemon, hp, atk, def, spa, spd, spe, helditems = string.match(line, RandomizerLog.Sectors.BaseStatsItems.PokemonBSTPattern)
+	local index = RandomizerLog.Sectors.BaseStatsItems.LineNumber + 1 -- remove the first line to skip the table header
+	while index <= #logLines do
+		local pokemon, hp, atk, def, spa, spd, spe, helditems = string.match(logLines[index] or "", RandomizerLog.Sectors.BaseStatsItems.PokemonBSTPattern)
 		pokemon = RandomizerLog.formatInput(pokemon)
 
 		-- If nothing matches, end of sector
 		if pokemon == nil or spe == nil or RandomizerLog.PokemonNameToIdMap[pokemon] == nil then
-			table.insert(logLines, 1, line)
 			return
 		end
 
 		local pokemonId = RandomizerLog.PokemonNameToIdMap[pokemon]
 		local pokemonData = RandomizerLog.Data.Pokemon[pokemonId]
 		if pokemonData ~= nil then
-			pokemonData.BaseStats = {}
-			pokemonData.BaseStats.hp = tonumber(hp) or 0
-			pokemonData.BaseStats.atk = tonumber(atk) or 0
-			pokemonData.BaseStats.def = tonumber(def) or 0
-			pokemonData.BaseStats.spa = tonumber(spa) or 0
-			pokemonData.BaseStats.spd = tonumber(spd) or 0
-			pokemonData.BaseStats.spe = tonumber(spe) or 0
+			pokemonData.BaseStats = {
+				hp = tonumber(hp) or 0,
+				atk = tonumber(atk) or 0,
+				def = tonumber(def) or 0,
+				spa = tonumber(spa) or 0,
+				spd = tonumber(spd) or 0,
+				spe = tonumber(spe) or 0,
+			}
 			if helditems ~= nil and helditems ~= "" then
 				pokemonData.BaseStats.helditems = RandomizerLog.formatInput(helditems)
 			end
 		end
-		line = table.remove(logLines, 1)
+		index = index + 1
 	end
 end
 
 function RandomizerLog.parseMoveSets(logLines)
-	local sectorHeader = RandomizerLog.Patterns.getSectorHeaderPattern(RandomizerLog.Sectors.MoveSets.Header)
-	if not RandomizerLog.seekSectorStart(logLines, sectorHeader) then
+	if RandomizerLog.Sectors.MoveSets.LineNumber == nil then
 		return
 	end
 
 	-- Parse the sector
-	local line = table.remove(logLines, 1)
-	while line ~= nil do
-		local pokemon = string.match(line, RandomizerLog.Sectors.MoveSets.NextMonPattern)
+	local index = RandomizerLog.Sectors.MoveSets.LineNumber
+	while index <= #logLines do
+		local pokemon = string.match(logLines[index] or "", RandomizerLog.Sectors.MoveSets.NextMonPattern)
 		pokemon = RandomizerLog.formatInput(pokemon)
 
 		-- Search for the next Pokémon's name, or the end of sector
+		-- I might fix this whole mess later when I'm awake
 		while pokemon == nil or RandomizerLog.PokemonNameToIdMap[pokemon] == nil do
-			if string.find(line, "TM Moves", 1, true) ~= nil then
-				table.insert(logLines, 1, line or "")
+			if string.find(logLines[index] or "", "^%-%-") ~= nil or index + 1 > #logLines then
 				return
 			end
-			line = table.remove(logLines, 1)
-			if line == nil then
-				return
-			end
-			pokemon = string.match(line, RandomizerLog.Sectors.MoveSets.NextMonPattern)
+			index = index + 1
+			pokemon = string.match(logLines[index] or "", RandomizerLog.Sectors.MoveSets.NextMonPattern)
 			pokemon = RandomizerLog.formatInput(pokemon)
 		end
 
@@ -293,17 +288,11 @@ function RandomizerLog.parseMoveSets(logLines)
 		local pokemonData = RandomizerLog.Data.Pokemon[pokemonId]
 		if pokemonData ~= nil then
 			pokemonData.MoveSet = {}
-			 -- First six lines are redundant Base Sets (also don't trust these will exist)
-			 table.remove(logLines, 1)
-			 table.remove(logLines, 1)
-			 table.remove(logLines, 1)
-			 table.remove(logLines, 1)
-			 table.remove(logLines, 1)
-			 table.remove(logLines, 1)
+			 -- First six lines are redundant Base Sets (also don't trust these will exist), and skip current line
+			 index = index + 7
 
 			-- Search for each listed level-up move
-			line = table.remove(logLines, 1)
-			local level, movename = string.match(line, RandomizerLog.Sectors.MoveSets.MovePattern)
+			local level, movename = string.match(logLines[index] or "", RandomizerLog.Sectors.MoveSets.MovePattern)
 			while level ~= nil and movename ~= nil do
 				local nextMove = {
 					level = tonumber(RandomizerLog.formatInput(level)) or 0,
@@ -311,59 +300,55 @@ function RandomizerLog.parseMoveSets(logLines)
 				}
 				table.insert(RandomizerLog.Data.Pokemon[pokemonId].MoveSet, nextMove)
 
-				line = table.remove(logLines, 1)
-				if line == nil then
+				index = index + 1
+				if index > #logLines then
 					return
 				end
-				level, movename = string.match(line, RandomizerLog.Sectors.MoveSets.MovePattern)
+				level, movename = string.match(logLines[index] or "", RandomizerLog.Sectors.MoveSets.MovePattern)
 			end
 		else
-			line = table.remove(logLines, 1)
+			index = index + 1
 		end
 	end
 end
 
 function RandomizerLog.parseTMMoves(logLines)
-	local sectorHeader = RandomizerLog.Patterns.getSectorHeaderPattern(RandomizerLog.Sectors.TMMoves.Header)
-	if not RandomizerLog.seekSectorStart(logLines, sectorHeader) then
+	if RandomizerLog.Sectors.TMMoves.LineNumber == nil then
 		return
 	end
 
 	-- Parse the sector
-	local line = table.remove(logLines, 1)
-	while line ~= nil do
-		local tmNumber, movename = string.match(line, RandomizerLog.Sectors.TMMoves.TMPattern)
+	local index = RandomizerLog.Sectors.TMMoves.LineNumber
+	while index <= #logLines do
+		local tmNumber, movename = string.match(logLines[index] or "", RandomizerLog.Sectors.TMMoves.TMPattern)
 		tmNumber = tonumber(RandomizerLog.formatInput(tmNumber) or "") -- nil if not a number
 		movename = RandomizerLog.formatInput(movename)
 
 		-- If nothing matches, end of sector
 		if tmNumber == nil or movename == nil or RandomizerLog.MoveNameToIdMap[movename] == nil then
-			table.insert(logLines, 1, line)
 			return
 		end
 
 		local moveId = RandomizerLog.MoveNameToIdMap[movename]
 		RandomizerLog.Data.TMs[tmNumber] = moveId
 
-		line = table.remove(logLines, 1)
+		index = index + 1
 	end
 end
 
 function RandomizerLog.parseTMCompatibility(logLines)
-	local sectorHeader = RandomizerLog.Patterns.getSectorHeaderPattern(RandomizerLog.Sectors.TMCompatibility.Header)
-	if not RandomizerLog.seekSectorStart(logLines, sectorHeader) then
+	if RandomizerLog.Sectors.TMCompatibility.LineNumber == nil then
 		return
 	end
 
 	-- Parse the sector
-	local line = table.remove(logLines, 1)
-	while line ~= nil do
-		local pokemon, tms = string.match(line, RandomizerLog.Sectors.TMCompatibility.NextMonPattern)
+	local index = RandomizerLog.Sectors.TMCompatibility.LineNumber
+	while index <= #logLines do
+		local pokemon, tms = string.match(logLines[index] or "", RandomizerLog.Sectors.TMCompatibility.NextMonPattern)
 		pokemon = RandomizerLog.formatInput(pokemon)
 
 		-- If nothing matches, end of sector
 		if pokemon == nil or tms == nil or RandomizerLog.PokemonNameToIdMap[pokemon] == nil then
-			table.insert(logLines, 1, line)
 			return
 		end
 
@@ -376,26 +361,24 @@ function RandomizerLog.parseTMCompatibility(logLines)
 				table.insert(RandomizerLog.Data.Pokemon[pokemonId].TMMoves, tmNumber)
 			end
 		end
-		line = table.remove(logLines, 1)
+		index = index + 1
 	end
 end
 
 function RandomizerLog.parseTrainers(logLines)
-	local sectorHeader = RandomizerLog.Patterns.getSectorHeaderPattern(RandomizerLog.Sectors.Trainers.Header)
-	if not RandomizerLog.seekSectorStart(logLines, sectorHeader) then
+	if RandomizerLog.Sectors.Trainers.LineNumber == nil then
 		return
 	end
 
 	-- Parse the sector
-	local line = table.remove(logLines, 1)
-	while line ~= nil do
-		local trainer_num, trainername, party = string.match(line, RandomizerLog.Sectors.Trainers.NextTrainerPattern)
+	local index = RandomizerLog.Sectors.Trainers.LineNumber
+	while index <= #logLines do
+		local trainer_num, trainername, party = string.match(logLines[index] or "", RandomizerLog.Sectors.Trainers.NextTrainerPattern)
 		trainer_num = tonumber(RandomizerLog.formatInput(trainer_num) or "") -- nil if not a number
 		trainername = RandomizerLog.formatInput(trainername)
 
 		-- If nothing matches, end of sector
 		if trainer_num == nil or trainername == nil or party == nil then
-			table.insert(logLines, 1, line)
 			return
 		end
 
@@ -422,17 +405,16 @@ function RandomizerLog.parseTrainers(logLines)
 				table.insert(RandomizerLog.Data.Trainers[trainer_num].party, partyPokemon)
 			end
 		end
-		line = table.remove(logLines, 1)
+		index = index + 1
 	end
 end
--- Utils.printDebug("#%s: %s >%s< %s", trainer_num, pokemon or "N/A", helditem or "N/A", level or 0)
 
 -- Currently unused
 function RandomizerLog.parsePickupItems(logLines)
-
+	-- Utils.printDebug("#%s: %s >%s< %s", trainer_num, pokemon or "N/A", helditem or "N/A", level or 0)
 end
 
--- DEBUG HELPER
+-- DEBUG HELPER, will use later for foreign games
 function RandomizerLog.printPokemonIds()
 	local count = 0
 	local mons = {}
@@ -454,7 +436,7 @@ function RandomizerLog.printPokemonIds()
 	end
 end
 
--- DEBUG HELPER
+-- DEBUG HELPER, will use later for foreign games
 function RandomizerLog.printMoveIds()
 	local count = 0
 	local moves = {}
