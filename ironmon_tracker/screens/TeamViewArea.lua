c23da6dd96156ba00059ca40dc90033d2c75ec28
Zac Elsik <shadowgripper@gmail.com>
2023-03-04 12:36:45 -0800

    Team View: now shows eggs

--- ironmon_tracker/screens/TeamViewArea.lua
+++ ironmon_tracker/screens/TeamViewArea.lua
@@ -1,6 +1,8 @@
 -- Displays the player's full team of six Pokémon below the gameplay/Tracker screen, in an expanded drawing space
 TeamViewArea = {
-	Labels = {},
+	Labels = {
+		eggNickname = "EGG",
+	},
 	Colors = {
 		text = "Lower box text",
 		border = "Lower box border",
@@ -43,8 +45,12 @@ function TeamViewArea.buildOutPartyScreen()
 	local boxWidth = math.floor(TeamViewArea.Canvas.width / 6)
 	local boxHeight = TeamViewArea.Canvas.height - 1
 	for i=1, 6, 1 do
-		local pokemon = Tracker.getPokemon(i, true)
-		if pokemon ~= nil and PokemonData.isValid(pokemon.pokemonID) then
+		-- Alternative "Tracker.getPokemon" to allow for eggs
+		local personality = Tracker.Data.ownTeam[i]
+		local pokemon = Tracker.Data.ownPokemon[personality or -1]
+		local validPokemon = pokemon ~= nil and (personality ~= 0 or (pokemon.trainerID ~= nil and pokemon.trainerID ~= 0))
+
+		if validPokemon and (PokemonData.isValid(pokemon.pokemonID) or pokemon.isEgg == 1) then
 			local partyMember = TeamViewArea.createPartyMemberBox(pokemon, nextBoxX, nextBoxY, boxWidth, boxHeight)
 			if partyMember ~= nil then
 				table.insert(TeamViewArea.PartyPokemon, partyMember)
@@ -58,6 +64,7 @@ function TeamViewArea.createPartyMemberBox(pokemon, x, y, width, height)
 	local barHeight = 3
 	local colOffset = 34
 	local finalboxOffset = Utils.inlineIf(x + width == Constants.SCREEN.WIDTH + Constants.SCREEN.RIGHT_GAP, -1, 0)
+	local isEgg = (pokemon.isEgg == 1)
 
 	local partyMember = {
 		x = x,
@@ -66,7 +73,7 @@ function TeamViewArea.createPartyMemberBox(pokemon, x, y, width, height)
 		height = height,
 		Buttons = {},
 		updateSelf = function(self)
-			-- Update colors
+			-- Update colors, necessary in case the theme changes between draws
 			self.text = Theme.COLORS["Default text"]
 			self.border = Theme.COLORS["Upper box border"]
 			self.fill = Theme.COLORS["Upper box background"]
@@ -81,26 +88,42 @@ function TeamViewArea.createPartyMemberBox(pokemon, x, y, width, height)
 			gui.drawRectangle(self.x, self.y, self.width + finalboxOffset, self.height, self.border, self.fill)
 			local yOffset = self.y
 
+			-- Pokemon's Nickname
+			local nicknameText = Utils.inlineIf(isEgg, TeamViewArea.Labels.eggNickname, pokemon.nickname)
+			Drawing.drawText(self.x + 1, yOffset, nicknameText, self.text, self.shadow)
+			yOffset = yOffset + Constants.SCREEN.LINESPACING - 1
+
+			if isEgg then
+				local iconset = Options.IconSetMap[Options["Pokemon icon set"]]
+				local eggIcon = FileManager.buildImagePath(iconset.folder, "412", iconset.extension) -- #412 = egg
+				if eggIcon ~= nil then
+					gui.drawImage(eggIcon, self.x + 1, yOffset - 7 + (iconset.yOffset or 0), 32, 32)
+				end
+			end
+
 			-- Draw all the clickable buttons
 			for _, button in pairs(self.Buttons) do
 				Drawing.drawButton(button, self.shadow)
 			end
 
-			-- Pokemon's Nickname
-			Drawing.drawText(self.x + 1, yOffset, pokemon.nickname, self.text, self.shadow)
-			yOffset = yOffset + Constants.SCREEN.LINESPACING - 1
-
 			-- Pokemon's Status (if any)
-			local status = MiscData.StatusCodeMap[pokemon.status] or ""
-			if pokemon.curHP <= 0 then
-				Drawing.drawStatusIcon(MiscData.StatusCodeMap[MiscData.StatusType.Faint], self.x + 16, yOffset + 1)
-			elseif status ~= MiscData.StatusCodeMap[MiscData.StatusType.None] then
-				Drawing.drawStatusIcon(status, self.x + 16, yOffset + 1)
+			if not isEgg then
+				local status = MiscData.StatusCodeMap[pokemon.status] or ""
+				if pokemon.curHP <= 0 then
+					Drawing.drawStatusIcon(MiscData.StatusCodeMap[MiscData.StatusType.Faint], self.x + 16, yOffset + 1)
+				elseif status ~= MiscData.StatusCodeMap[MiscData.StatusType.None] then
+					Drawing.drawStatusIcon(status, self.x + 16, yOffset + 1)
+				end
 			end
 
 			-- Pokemon Types
 			yOffset = yOffset + 2
-			local types = PokemonData.Pokemon[pokemon.pokemonID].types
+			local types
+			if isEgg then
+				types = { PokemonData.Types.UNKNOWN, PokemonData.Types.UNKNOWN, }
+			else
+				types = PokemonData.Pokemon[pokemon.pokemonID].types
+			end
 			Drawing.drawTypeIcon(types[1], self.x + colOffset, yOffset)
 			yOffset = yOffset + 12
 			if types[2] ~= types[1] then
@@ -109,34 +132,37 @@ function TeamViewArea.createPartyMemberBox(pokemon, x, y, width, height)
 			yOffset = yOffset + 13
 
 			-- Pokemon LevelPattern
-			local levelText = string.format("Lv.%s", pokemon.level or 0)
+			local levelValue = Utils.inlineIf(isEgg, Constants.HIDDEN_INFO, pokemon.level or 0)
+			local levelText = string.format("Lv.%s", levelValue)
 			Drawing.drawText(self.x + 1, yOffset, levelText, self.text, self.shadow)
 			yOffset = yOffset + 2
 
 			-- Pokemon HP Bar
-			local hpPercentage = (pokemon.curHP or 0) / (pokemon.stats.hp or 100)
-			local hpBarColors = { Theme.COLORS["Positive text"], self.border, self.fill }
-			if hpPercentage >= 0.50 then
-				hpBarColors[1] = Theme.COLORS["Positive text"]
-			elseif hpPercentage >= 0.20 then
-				hpBarColors[1] = Theme.COLORS["Intermediate text"]
-			else
-				hpBarColors[1] = Theme.COLORS["Negative text"]
-			end
-			Drawing.drawPercentageBar(self.x + colOffset, yOffset, self.width - colOffset - 2, barHeight, hpPercentage, hpBarColors)
-			yOffset = yOffset + barHeight + 0
+			if not isEgg then
+				local hpPercentage = (pokemon.curHP or 0) / (pokemon.stats.hp or 100)
+				local hpBarColors = { Theme.COLORS["Positive text"], self.border, self.fill }
+				if hpPercentage >= 0.50 then
+					hpBarColors[1] = Theme.COLORS["Positive text"]
+				elseif hpPercentage >= 0.20 then
+					hpBarColors[1] = Theme.COLORS["Intermediate text"]
+				else
+					hpBarColors[1] = Theme.COLORS["Negative text"]
+				end
+				Drawing.drawPercentageBar(self.x + colOffset, yOffset, self.width - colOffset - 2, barHeight, hpPercentage, hpBarColors)
+				yOffset = yOffset + barHeight + 0
 
-			-- Pokemon EXP Bar
-			local expPercentage = (pokemon.currentExp or 0) / (pokemon.totalExp or 100)
-			local expBarColors = { self.text, self.border, self.fill }
-			Drawing.drawPercentageBar(self.x + colOffset, yOffset, self.width - colOffset - 2, barHeight, expPercentage, expBarColors)
-			yOffset = yOffset + barHeight + 2
+				-- Pokemon EXP Bar
+				local expPercentage = (pokemon.currentExp or 0) / (pokemon.totalExp or 100)
+				local expBarColors = { self.text, self.border, self.fill }
+				Drawing.drawPercentageBar(self.x + colOffset, yOffset, self.width - colOffset - 2, barHeight, expPercentage, expBarColors)
+				yOffset = yOffset + barHeight + 2
+			end
 		end,
 	}
 
 	local yOffset = y + Constants.SCREEN.LINESPACING
 	local iconBtn = {
-		pokemonID = pokemon.pokemonID,
+		pokemonID = Utils.inlineIf(isEgg, 412, pokemon.pokemonID), -- #412 = egg
 		type = Constants.ButtonTypes.POKEMON_ICON,
 		getIconPath = function(self)
 			local iconset = Options.IconSetMap[Options["Pokemon icon set"]]
@@ -145,37 +171,45 @@ function TeamViewArea.createPartyMemberBox(pokemon, x, y, width, height)
 		clickableArea = { x + 1, yOffset, 32, 27 },
 		box = { x + 1, yOffset - 7, 32, 32 },
 		onClick = function(self)
-			if PokemonData.isValid(self.pokemonID) then
+			if not isEgg and PokemonData.isValid(self.pokemonID) then
 				InfoScreen.changeScreenView(InfoScreen.Screens.POKEMON_INFO, self.pokemonID)
 			end
 		end
 	}
 	table.insert(partyMember.Buttons, iconBtn)
 
-	local typeDefensesBtn = {
-		-- Invisible button area for the type defenses boxes
-		pokemonID = pokemon.pokemonID,
-		type = Constants.ButtonTypes.NO_BORDER,
-		box = { x + colOffset, yOffset, 30, 24, },
-		onClick = function (self)
-			if PokemonData.isValid(self.pokemonID) then
-				TypeDefensesScreen.buildOutPagedButtons(self.pokemonID)
-				Program.changeScreenView(TypeDefensesScreen)
-			end
-		end,
-	}
-	table.insert(partyMember.Buttons, typeDefensesBtn)
+	if not isEgg then
+		local typeDefensesBtn = {
+			-- Invisible button area for the type defenses boxes
+			pokemonID = pokemon.pokemonID,
+			type = Constants.ButtonTypes.NO_BORDER,
+			box = { x + colOffset, yOffset, 30, 24, },
+			onClick = function (self)
+				if PokemonData.isValid(self.pokemonID) then
+					TypeDefensesScreen.buildOutPagedButtons(self.pokemonID)
+					Program.changeScreenView(TypeDefensesScreen)
+				end
+			end,
+		}
+		table.insert(partyMember.Buttons, typeDefensesBtn)
+	end
 
 	-- Pokemon Item
 	yOffset = yOffset + Constants.SCREEN.LINESPACING + 25
+	local itemText
+	if isEgg then
+		itemText = Constants.BLANKLINE
+	else
+		itemText = MiscData.Items[pokemon.heldItem or 0] or Constants.BLANKLINE
+	end
 	local itemBtn = {
-		text = MiscData.Items[pokemon.heldItem or 0] or Constants.BLANKLINE,
-		itemId = pokemon.heldItem or 0,
+		text = itemText,
+		itemId = Utils.inlineIf(isEgg, 0, pokemon.heldItem or 0),
 		type = Constants.ButtonTypes.NO_BORDER,
 		box = { x, yOffset, partyMember.width - 2, Constants.SCREEN.LINESPACING, },
 		onClick = function (self)
 			-- Implement sometime in the future
-			-- if self.itemId ~= nil and self.itemId ~= 0 then
+			-- if not isEgg and self.itemId ~= nil and self.itemId ~= 0 then
 			-- 	InfoScreen.changeScreenView(InfoScreen.Screens.ITEM_INFO, self.itemId)
 			-- end
 		end,
@@ -186,18 +220,18 @@ function TeamViewArea.createPartyMemberBox(pokemon, x, y, width, height)
 	yOffset = yOffset + Constants.SCREEN.LINESPACING - 1
 	local abilityText
 	local abilityId = PokemonData.getAbilityId(pokemon.pokemonID, pokemon.abilityNum)
-	if abilityId ~= nil and abilityId ~= 0 then
+	if not isEgg and abilityId ~= nil and abilityId ~= 0 then
 		abilityText = AbilityData.Abilities[abilityId].name
 	else
 		abilityText = Constants.BLANKLINE
 	end
 	local abilityBtn = {
 		text = abilityText,
-		abilityId = abilityId,
+		abilityId = Utils.inlineIf(isEgg, 0, abilityId or 0),
 		type = Constants.ButtonTypes.NO_BORDER,
 		box = { x, yOffset, partyMember.width - 2, Constants.SCREEN.LINESPACING, },
 		onClick = function (self)
-			if self.abilityId ~= nil and self.abilityId ~= 0 then
+			if not isEgg and self.abilityId ~= nil and self.abilityId ~= 0 then
 				InfoScreen.changeScreenView(InfoScreen.Screens.ABILITY_INFO, self.abilityId)
 			end
 		end,
