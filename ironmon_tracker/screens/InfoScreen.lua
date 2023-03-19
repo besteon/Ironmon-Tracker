62da91ae9eb515859d41c152cf993f8f275e3fe3
Zac Elsik <shadowgripper@gmail.com>
2023-03-18 19:48:36 -0700

    Route Data levels visible for FRLG and early RSE

--- ironmon_tracker/screens/InfoScreen.lua
+++ ironmon_tracker/screens/InfoScreen.lua
@@ -102,18 +102,35 @@ InfoScreen.Buttons = {
 		isVisible = function() return InfoScreen.viewScreen == InfoScreen.Screens.ROUTE_INFO end,
 		onClick = function(self) InfoScreen.openRouteInfoWindow() end
 	},
-	ShowOriginalRoute = {
+	ShowRoutePercentages = {
 		type = Constants.ButtonTypes.CHECKBOX,
-		text = "Show original route data",
+		text = "Percentages",
 		textColor = "Default text",
-		clickableArea = { Constants.SCREEN.WIDTH + Constants.SCREEN.MARGIN + 14, Constants.SCREEN.MARGIN + 17, 104, 10 },
-		box = { Constants.SCREEN.WIDTH + Constants.SCREEN.MARGIN + 14, Constants.SCREEN.MARGIN + 18, 8, 8 },
+		clickableArea = { Constants.SCREEN.WIDTH + Constants.SCREEN.MARGIN + 15, Constants.SCREEN.MARGIN + 17, 61, 10 },
+		box = { Constants.SCREEN.WIDTH + Constants.SCREEN.MARGIN + 15, Constants.SCREEN.MARGIN + 18, 8, 8 },
 		boxColors = { "Upper box border", "Upper box background" },
-		toggleState = false, -- When true, the original game data for the route is revealed
+		toggleState = false, -- When true, the original game percentage rates for the route are revealed
 		toggleColor = "Positive text",
 		isVisible = function() return InfoScreen.viewScreen == InfoScreen.Screens.ROUTE_INFO end,
 		onClick = function(self)
 			self.toggleState = not self.toggleState
+			InfoScreen.Buttons.ShowRouteLevels.toggleState = false
+			Program.redraw(true)
+		end
+	},
+	ShowRouteLevels = {
+		type = Constants.ButtonTypes.CHECKBOX,
+		text = "Levels",
+		textColor = "Default text",
+		clickableArea = { Constants.SCREEN.WIDTH + Constants.SCREEN.MARGIN + 88, Constants.SCREEN.MARGIN + 17, 36, 10 },
+		box = { Constants.SCREEN.WIDTH + Constants.SCREEN.MARGIN + 88, Constants.SCREEN.MARGIN + 18, 8, 8 },
+		boxColors = { "Upper box border", "Upper box background" },
+		toggleState = false, -- When true, the original game Pokemon levels for the route are revealed
+		toggleColor = "Positive text",
+		isVisible = function() return InfoScreen.viewScreen == InfoScreen.Screens.ROUTE_INFO end,
+		onClick = function(self)
+			self.toggleState = not self.toggleState
+			InfoScreen.Buttons.ShowRoutePercentages.toggleState = false
 			Program.redraw(true)
 		end
 	},
@@ -265,7 +282,8 @@ function InfoScreen.clearScreenData()
 	InfoScreen.prevScreen = 0
 	InfoScreen.infoLookup = 0
 	InfoScreen.prevScreenInfo = 0
-	InfoScreen.Buttons.ShowOriginalRoute.toggleState = false
+	InfoScreen.Buttons.ShowRoutePercentages.toggleState = false
+	InfoScreen.Buttons.ShowRouteLevels.toggleState = false
 end
 
 -- Display a Pokemon that is 'N' entries ahead of the currently shown Pokemon; N can be negative
@@ -435,7 +453,8 @@ function InfoScreen.openRouteInfoWindow()
 
 			InfoScreen.infoLookup.mapId = mapId
 			InfoScreen.infoLookup.encounterArea = encounterArea
-			InfoScreen.Buttons.ShowOriginalRoute.toggleState = false
+			InfoScreen.Buttons.ShowRoutePercentages.toggleState = false
+			InfoScreen.Buttons.ShowRouteLevels.toggleState = false
 			Program.redraw(true)
 		end
 		client.unpause()
@@ -448,7 +467,7 @@ function InfoScreen.getPokemonButtonsForEncounterArea(mapId, encounterArea)
 
 	local areaInfo
 	local totalPossible
-	if InfoScreen.Buttons.ShowOriginalRoute.toggleState then
+	if InfoScreen.Buttons.ShowRoutePercentages.toggleState or InfoScreen.Buttons.ShowRouteLevels.toggleState then
 		areaInfo = RouteData.getEncounterAreaPokemon(mapId, encounterArea)
 		totalPossible = #areaInfo
 	else
@@ -473,9 +492,12 @@ function InfoScreen.getPokemonButtonsForEncounterArea(mapId, encounterArea)
 	for index=1, totalPossible, 1 do
 		local pokemonID = 252 -- Question mark icon
 		local rate = nil
+		local minLv, maxLv = nil, nil
 		if areaInfo ~= nil and areaInfo[index] ~= nil then
 			pokemonID = areaInfo[index].pokemonID
 			rate = areaInfo[index].rate
+			minLv = areaInfo[index].minLv
+			maxLv = areaInfo[index].maxLv
 		end
 
 		local x = startX + offsetX
@@ -489,6 +511,8 @@ function InfoScreen.getPokemonButtonsForEncounterArea(mapId, encounterArea)
 			end,
 			pokemonID = pokemonID,
 			rate = rate,
+			minLv = minLv,
+			maxLv = maxLv,
 			box = { x, y, iconWidth, iconWidth },
 			isVisible = function() return InfoScreen.viewScreen == InfoScreen.Screens.ROUTE_INFO end,
 			onClick = function(self)
@@ -932,7 +956,8 @@ function InfoScreen.drawRouteInfoScreen(mapId, encounterArea)
 	Drawing.drawImageAsPixels(Constants.PixelImages.MAP_PINDROP, boxX + 3, boxTopY + 3, { Theme.COLORS["Default text"] }, boxTopShadow)
 	Drawing.drawText(boxX + 13, boxTopY + 2, routeName, Theme.COLORS["Default text"], boxTopShadow)
 
-	Drawing.drawButton(InfoScreen.Buttons.ShowOriginalRoute, boxTopShadow)
+	Drawing.drawButton(InfoScreen.Buttons.ShowRoutePercentages, boxTopShadow)
+	Drawing.drawButton(InfoScreen.Buttons.ShowRouteLevels, boxTopShadow)
 
 	-- BOT BOX VIEW
 	gui.defaultTextBackground(Theme.COLORS["Lower box background"])
@@ -943,12 +968,11 @@ function InfoScreen.drawRouteInfoScreen(mapId, encounterArea)
 	Drawing.drawText(boxX + 10, botBoxY - 11, encounterHeaderText, Theme.COLORS["Header text"], bgHeaderShadow)
 	gui.drawRectangle(boxX, botBoxY, boxWidth, botBoxHeight, Theme.COLORS["Lower box border"], Theme.COLORS["Lower box background"])
 
-	if not InfoScreen.Buttons.ShowOriginalRoute.toggleState then
+	if not (InfoScreen.Buttons.ShowRoutePercentages.toggleState or InfoScreen.Buttons.ShowRouteLevels.toggleState) then
 		Drawing.drawText(boxX + 2, botBoxY, "In order of appearance:", Theme.COLORS["Lower box text"], boxBotShadow)
 	end
 
 	-- POKEMON SEEN
-
 	for _, iconButton in pairs(InfoScreen.TemporaryButtons) do
 		if iconButton.pokemonID == 252--[[ Question mark icon]] and Options.IconSetMap[Options["Pokemon icon set"]].adjustQuestionMark then
 			iconButton.box[2] = iconButton.box[2] + Options.IconSetMap[Options["Pokemon icon set"]].yOffset
@@ -958,11 +982,20 @@ function InfoScreen.drawRouteInfoScreen(mapId, encounterArea)
 		local y = iconButton.box[2]
 		Drawing.drawButton(iconButton, boxBotShadow)
 
-		if iconButton.rate ~= nil then
-			local rateText = math.floor(iconButton.rate * 100) .. "%"
-			local rateOffset = Utils.inlineIf(iconButton.rate == 1.00, 5, Utils.inlineIf(iconButton.rate >= 0.1, 7, 9)) -- centering
+		local iconInfoText = nil
+		if InfoScreen.Buttons.ShowRoutePercentages.toggleState and iconButton.rate ~= nil then
+			iconInfoText = math.floor(iconButton.rate * 100) .. "%"
+		elseif InfoScreen.Buttons.ShowRouteLevels.toggleState and iconButton.minLv ~= nil and iconButton.maxLv ~= nil then
+			if iconButton.minLv == 0 and iconButton.maxLv == 0 then
+				iconInfoText = Constants.HIDDEN_INFO
+			else
+				iconInfoText = string.format("%s - %s", math.floor(iconButton.minLv), math.floor(iconButton.maxLv))
+			end
+		end
+		if iconInfoText ~= nil then
+			local iconInfoOffsetX = Utils.getCenteredTextX(iconInfoText, 30)
 			gui.drawRectangle(x + 1, y, 30, 8, Theme.COLORS["Lower box background"], Theme.COLORS["Lower box background"])
-			Drawing.drawText(x + rateOffset, y - 1, rateText, Theme.COLORS["Lower box text"], boxBotShadow)
+			Drawing.drawText(x + iconInfoOffsetX, y - 1, iconInfoText, Theme.COLORS["Lower box text"], boxBotShadow)
 		end
 	end
 
