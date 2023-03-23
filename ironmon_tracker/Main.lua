dd4d07fee4c28debaf453001629f97a6b235e240
Zac Elsik <shadowgripper@gmail.com>
2023-03-22 19:16:14 -0700

    7.5.1 Patch
    Applying necessary fix for log viewer for Premade Roms.

--- ironmon_tracker/Main.lua
+++ ironmon_tracker/Main.lua
@@ -1,7 +1,7 @@
 Main = {}
 
 -- The latest version of the tracker. Should be updated with each PR.
-Main.Version = { major = "7", minor = "5", patch = "0" }
+Main.Version = { major = "7", minor = "5", patch = "1" }
 
 Main.CreditsList = { -- based on the PokemonBizhawkLua project by MKDasher
 	CreatedBy = "Besteon",
