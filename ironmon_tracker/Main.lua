591a3424072e907f3955b6bcef641040a30924d2
Zac Elsik <shadowgripper@gmail.com>
2023-03-23 14:39:01 -0700

    7.5.2 Patch

--- ironmon_tracker/Main.lua
+++ ironmon_tracker/Main.lua
@@ -1,7 +1,7 @@
 Main = {}
 
 -- The latest version of the tracker. Should be updated with each PR.
-Main.Version = { major = "7", minor = "5", patch = "1" }
+Main.Version = { major = "7", minor = "5", patch = "2" }
 
 Main.CreditsList = { -- based on the PokemonBizhawkLua project by MKDasher
 	CreatedBy = "Besteon",
