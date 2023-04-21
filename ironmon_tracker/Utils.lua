fb75fda4e03f2836b509b72ee3fb9eb83c82e736
Zac Elsik <shadowgripper@gmail.com>
2023-04-21 10:59:23 -0700

    Utils: add diagnostic to bypass error check

--- ironmon_tracker/Utils.lua
+++ ironmon_tracker/Utils.lua
@@ -154,9 +154,13 @@ function Utils.formatSpecialCharacters(text)
 	return text
 end
 
+-- Checks if the text starts with a Japanese or Chinese character. Only works on Lua 5.4+
 function Utils.startsWithJapaneseChineseChar(text)
-	if text == nil or not Main.supportsSpecialChars then return false end
+	if text == nil or not Main.supportsSpecialChars then
+		return false
+	end
 
+	---@diagnostic disable-next-line: err-esc
 	local pattern = "^[\u{3040}-\u{30ff}\u{3400}-\u{4dbf}\u{4e00}-\u{9fff}\u{f900}-\u{faff}\u{ff66}-\u{ff9f}]"
 	return string.match(text, pattern) ~= nil
 end
