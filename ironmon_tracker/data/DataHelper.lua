24f9dd5ad6ae318487941ed5ac15b5d40d910393
UTDZac <shadowgripper@gmail.com>
2023-10-03 18:24:48 -0700

    Don't use "nickname" if same as in-game name
    The "nickname" letters are not formatted as well, as they're read directly from the game memory. If the nickname is the same as the pokemon's name, simply use the better-formatted pokemon name as-is.

--- ironmon_tracker/data/DataHelper.lua
+++ ironmon_tracker/data/DataHelper.lua
@@ -126,7 +126,7 @@ function DataHelper.buildTrackerScreenDisplay(forceView)
 	-- POKEMON ITSELF (data.p)
 	data.p.id = viewedPokemon.pokemonID
 	data.p.name = viewedPokemon.name or Constants.BLANKLINE
-	if Options["Show nicknames"] and viewedPokemon.nickname and viewedPokemon.nickname ~= "" then
+	if Options["Show nicknames"] and viewedPokemon.nickname ~= "" and Utils.toLowerUTF8(viewedPokemon.name) ~= Utils.toLowerUTF8(viewedPokemon.nickname) then
 		data.p.name = Utils.formatSpecialCharacters(viewedPokemon.nickname)
 	end
 	data.p.curHP = viewedPokemon.curHP or Constants.BLANKLINE
