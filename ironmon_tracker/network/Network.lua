f0bd549806d5d99fb6a2a3ccbe20f89444fb90d4
UTDZac <shadowgripper@gmail.com>
2023-12-05 13:28:04 -0800

    Network: Fix commands input lowercase
    The entire input was being converted to lowercase instead of just the command part.

--- ironmon_tracker/network/Network.lua
+++ ironmon_tracker/network/Network.lua
@@ -1,7 +1,7 @@
 Network = {
 	CurrentConnection = {},
 	lastUpdateTime = 0,
-	STREAMERBOT_VERSION = "1.0.4", -- Known streamerbot version. Update this value to inform user to update streamerbot code
+	STREAMERBOT_VERSION = "1.0.5", -- Known streamerbot version. Update this value to inform user to update streamerbot code
 	TEXT_UPDATE_FREQUENCY = 2, -- # of seconds
 	SOCKET_UPDATE_FREQUENCY = 2, -- # of seconds
 	HTTP_UPDATE_FREQUENCY = 2, -- # of seconds
