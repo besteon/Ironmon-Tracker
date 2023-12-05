cbfbe3dc4b86367be5c9401f988007bb2e7e4e8c
UTDZac <shadowgripper@gmail.com>
2023-12-05 13:05:44 -0800

    Fix Event creation defaulting to enabled:true
    Regardless of "IsEnabled" parameter being specified, it would always default to true.

--- ironmon_tracker/network/EventHandler.lua
+++ ironmon_tracker/network/EventHandler.lua
@@ -905,7 +905,9 @@ function EventHandler.IEvent:new(o)
 	o = o or {}
 	o.Key = o.Key or EventHandler.Events.None.Key
 	o.Type = o.Type or EventHandler.EventTypes.None
-	o.IsEnabled = o.IsEnabled ~= nil and o.IsEnabled or true
+	if o.IsEnabled == nil then
+		o.IsEnabled = true
+	end
 	setmetatable(o, self)
 	self.__index = self
 	return o
