1fd369007951740fd343b1d7a7a4a8607b0f452f
UTDZac <shadowgripper@gmail.com>
2024-03-16 12:10:13 -0700

    Update to 8.4.3 Release

--- ironmon_tracker/Main.lua
+++ ironmon_tracker/Main.lua
@@ -1,7 +1,7 @@
 Main = {}
 
 -- The latest version of the tracker. Should be updated with each PR.
-Main.Version = { major = "8", minor = "4", patch = "2" }
+Main.Version = { major = "8", minor = "4", patch = "3" }
 
 Main.CreditsList = { -- based on the PokemonBizhawkLua project by MKDasher
 	CreatedBy = "Besteon",
