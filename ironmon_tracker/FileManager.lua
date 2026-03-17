e14ea091fbfba12dd762739def8c8fb5906e1d05
UTDZac (Zeke) <shadowgripper@gmail.com>
2026-03-17 16:53:45 -0700

    Move trainer route data to its own data file

--- ironmon_tracker/FileManager.lua
+++ ironmon_tracker/FileManager.lua
@@ -196,6 +196,21 @@ FileManager.LuaCode = {
 	{ name = "CustomCode", filepath = "CustomCode.lua", },
 }
 
+-- Data files that can be loaded at runtime when needed (they aren't available until loaded)
+-- The table key is the versioncolor, and the value is a table of data key labels and their filepaths
+FileManager.LuaData = {
+	Ruby = {},
+	Sapphire = {},
+	Emerald = {},
+	FireRed = {
+		TrainerRoutes = FileManager.Folders.DataCode .. FileManager.slash .. "FRLGTrainerRouteData.lua",
+	},
+	LeafGreen = {
+		TrainerRoutes = FileManager.Folders.DataCode .. FileManager.slash .. "FRLGTrainerRouteData.lua",
+	},
+	All = {},
+}
+
 -- Some files are initialized early and don't need to be re-intialized again
 FileManager.ExcludeFromInitialize = {
 	["UpdateOrInstall"] = true,
@@ -408,6 +423,22 @@ function FileManager.loadLuaFile(filename, silenceErrors)
 	end
 
 	return false
+end
+
+---Loads a known data file and returns the result.
+---@param gameKey string Ruby, Sapphire, Emerald, FireRed, LeafGreen, or All
+---@param dataKey string The key label for the data file to load
+---@return any|nil
+function FileManager.loadLuaData(gameKey, dataKey)
+	local gameDataTable = FileManager.LuaData[gameKey or false] or {}
+	local dataFilepath = gameDataTable[dataKey or false] or ""
+
+	local filepath = FileManager.getPathIfExists(FileManager.Folders.TrackerCode .. FileManager.slash .. dataFilepath)
+	if not filepath then
+		return nil
+	end
+
+	return dofile(filepath)
 end
 
 ---Executes 'functionName' for all loaded code files.
